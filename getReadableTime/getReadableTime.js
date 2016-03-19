d2a40f9f2787433ae69785eab57139ae20bff463
paola robles-desgarennes <paola robles-desgarennes@mac.com>
2016-03-19 13:02:32 -0700

    my answer to the amazing excersise that ycegul create

--- getReadableTime/getReadableTime.js
+++ getReadableTime/getReadableTime.js
@@ -12,7 +12,7 @@ Bonus 2: return -1 if the input is not a number or if
 it is a negative number
 */
 
-/**
+/** 
 Examples:
 
 var result = getReadableTime(102);
@@ -34,13 +34,37 @@ var result = getReadableTime();
 console.log(result) // -1
 
 var result = getReadableTime(-12);
-console.log(result) // -1
+console.Given a number of minutes as an integer, return an object with two properties,
+"hour" and "minute", which is equivalent to the input minutes.
+Hint: Check out Math.floor function at
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+Hint: Check out remainder operator at
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder
+
+Bonus 1: Add "day" property
+
+Bonus 2: return -1 if the input is not a number or if 
+it is a negative numberlog(result) // -1
 
 **/
 
 
 
 function getReadableTime(minutes) {
-	//Your code here
-	
-}
+	var time= {
+		day:0;
+		hour:0;
+		minute:0;
+
+	}
+	var addHour= math.floor(minutes/60);
+	var addMinutes= minutes - (addHour*60);
+	time.hour=addHour;
+	time.minute= addMinute;
+	console.log(time);
+
+};
+
+    var totalHour= Math.floor(minutes/60)
+    var
+
