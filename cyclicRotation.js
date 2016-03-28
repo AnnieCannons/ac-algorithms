538f33ea5749d02e5483b2ddc4d57d6626a7e559
Your Name <you@example.com>
2016-03-28 10:39:29 -0700

    JLH answer to cyclicRotation

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -2,18 +2,34 @@
 // * Created by yoneta on 3/23/16.
 // */
 
-//A zero-indexed array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is also moved to the first place.
+//A zero-indexed array A consisting of N integers is given.
+// Rotation of the array means that each element is shifted right by one index, 
+//and the last element of the array is also moved to the first place.
 //
-//    For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]. The goal is to rotate array A K times; that is, each element of A will be shifted to the right by K indexes.
+//    For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]. 
+//The goal is to rotate array A K times; that is, 
+//each element of A will be shifted to the right by K indexes.
 //
 //    Write a function:
 //
 //function cyclicRotation(A, K);
 //
-//that, given a zero-indexed array A consisting of N integers and an integer K, returns the array A rotated K times.
+//that, given a zero-indexed array A consisting of N integers and an integer K, 
+//returns the array A rotated K times.
 //
 //    For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].\
 
 function cyclicRotation(arr, rotation){
-    //Your code here...
+
+    //take the step for each integer up to K
+    // and place as first item in array
+
+    for (i = 0; i<rotation ; i++){
+    	//remove last item in array and store as variable
+    	var popped = arr.pop();
+    	//add stored variable as first item in arracy	
+    	arr.unshift(popped);
+    }
+    //return resulting array
+    return arr;
 }
