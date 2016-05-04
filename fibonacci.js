5d222f51e5205aeebd266607b48fd5604730f8df
Your Name <you@example.com>
2016-05-04 09:25:55 -0700

    JLH answers to Fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -8,10 +8,30 @@
 
 
 function sumFibonacci(num) {
-  //your code here
+  //input num
+  var a = 0;
+  var b = 1;
+  var total = 0;
+  //confirm num is greater than 1
+  if (num < 1){
+  	console.log("Too small");
+    return 1;
+  } else {
+  	//collect all fibonacci numbers under num in the variable total
+    while (b <= num) {
+      if (b%2 === 1){
+        total += b;
+      }
+      b = a + b;
+      a = b - a;
+    }
+       console.log(total);
+  	    return total; 
+  }
+  	//output sum of fibonaccis
+    console.log(total);
+  	    return total; 
 }
-
-
 
 //TEST CASES
 //sumFibonacci(1000); should return 1785.
