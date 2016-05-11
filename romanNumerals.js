c0c4bf5c4b23af61fa62b7422fa7843b3c0dff16
Your Name <you@example.com>
2016-05-11 10:17:52 -0700

    JLH answer to RomanNumeral exercise

--- romanNumerals.js
+++ romanNumerals.js
@@ -15,7 +15,159 @@
 //4)Array.join() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 
 function speakRoman(num) {
- 	//your code here
+ 	//create variable for remainder
+ 	var result =[];
+ 	var thousands, hundreds, tens, ones;
+ 	//find thousands in num and remainder;
+ 	var rem = num%1000;
+ 	//push # of thousands into result array
+ 	thousands = (num-rem)/1000;
+ 	if (thousands > 0) {
+ 		switch (thousands) {
+ 		case 10:
+ 			result.push("_X");
+ 			break;
+ 		case 9:
+ 			result.push("_XM");
+ 			break;
+ 		case 8:
+ 			result.push("VMMM");
+ 			break;
+ 		case 7:
+ 			result.push("VMM");
+ 			break;
+ 		case 6:
+ 			result.push("VMM");
+ 			break;
+ 		case 5:
+ 			result.push("V");
+ 			break;
+ 		case 4:
+ 			result.push("MV");
+ 			break;
+ 		case 3:
+ 			result.push("MMM");
+ 			break;
+ 		case 2:
+ 			result.push("MM");
+ 			break;
+ 		case 1:
+ 			result.push("M");
+ 			break;								
+ 		}
+ 	}
+ 	//find hundreds in remainder and reset remainder
+ 	hundreds = (rem - (rem%100))/100;
+ 	rem = rem%100;
+ 	//push # of hundreds into result array
+ 	if (hundreds > 0) {
+ 		switch (hundreds) {
+ 		
+ 		case 9:
+ 			result.push("CM");
+ 			break;
+ 		case 8:
+ 			result.push("DCCC");
+ 			break;
+ 		case 7:
+ 			result.push("DCC");
+ 			break;
+ 		case 6:
+ 			result.push("DC");
+ 			break;
+ 		case 5:
+ 			result.push("D");
+ 			break;
+ 		case 4:
+ 			result.push("CD");
+ 			break;
+ 		case 3:
+ 			result.push("CCC");
+ 			break;
+ 		case 2:
+ 			result.push("CC");
+ 			break;
+ 		case 1:
+ 			result.push("C");
+ 			break;								
+ 		}
+ 	}
+ 	//find tens in remainder and reset remainder
+ 	tens = (rem - (rem%10))/10;
+ 	rem = rem%10;
+ 	//push # of tens into result array
+ 	if (tens > 0) {
+ 		switch (tens) {
+ 		
+ 		case 9:
+ 			result.push("XC");
+ 			break;
+ 		case 8:
+ 			result.push("LXXX");
+ 			break;
+ 		case 7:
+ 			result.push("LXX");
+ 			break;
+ 		case 6:
+ 			result.push("LX");
+ 			break;
+ 		case 5:
+ 			result.push("L");
+ 			break;
+ 		case 4:
+ 			result.push("XL");
+ 			break;
+ 		case 3:
+ 			result.push("XXX");
+ 			break;
+ 		case 2:
+ 			result.push("XX");
+ 			break;
+ 		case 1:
+ 			result.push("X");
+ 			break;								
+ 		}
+ 	}
+ 	//find ones in remainder
+ 	ones = rem;
+ 	//push # of ones into result array
+ 	if (ones > 0) {
+ 		switch (ones) {
+ 		
+ 		case 9:
+ 			result.push("IX");
+ 			break;
+ 		case 8:
+ 			result.push("VIII");
+ 			break;
+ 		case 7:
+ 			result.push("VII");
+ 			break;
+ 		case 6:
+ 			result.push("VI");
+ 			break;
+ 		case 5:
+ 			result.push("V");
+ 			break;
+ 		case 4:
+ 			result.push("IV");
+ 			break;
+ 		case 3:
+ 			result.push("III");
+ 			break;
+ 		case 2:
+ 			result.push("II");
+ 			break;
+ 		case 1:
+ 			result.push("I");
+ 			break;								
+ 		}
+ 	}
+ 	//join result array
+ 	var roman = result.join("");
+ 	console.log(roman);
+ 	return roman;
+}
 }
 
 speakRoman(2); // "II".
